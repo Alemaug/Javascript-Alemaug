2b5944d3661444f2f91d736e26820ad7f3c88602
Alemaug <alejandro.gordo@live.com.ar>
2022-08-01 22:17:32 -0300

    practica con dom

---
+++ Practica con DOM.js
@@ -0,0 +1,9 @@
+ console.log('Practica con DOM');
+
+ document.querySelector('h3')
+ console.log(document.querySelector('h3'));
+ console.log(document.querySelector('.h3-2'));
+ console.log(document.querySelector('#parrafo'));
+ console.log(document.getElementById('parrafo'));
+
+ console.log(document.querySelectorAll('.h3-2'));
